3afd5c160ecb946851e75fcc5a37b088eca8915a
Simone Rotondo <simone.rotondo@gmail.com>
2017-07-10 15:51:32 +0200

    Drakvuf service

---
+++ services/drakvuf/drakvuf/drakvuf.go
@@ -0,0 +1,138 @@
+package drakvuf
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Drakvuf struct {
+	IncomingDir   string
+	ProcessingDir string
+	FinishedDir   string
+	PendingTasks  map[string]string
+}
+
+type StatusResp struct {
+	PendingTaskNum int `json:"pendingtasknum"`
+}
+
+type TasksReport struct {
+	TaskID string `json;"taskid"`
+	Log    []byte `json:"log"`
+}
+
+// Drakvuf constructor
+func New(incomingDir string, processingDir string, finishedDir string) (*Drakvuf, error) {
+	r := &Drakvuf{
+		IncomingDir:   incomingDir,
+		ProcessingDir: processingDir,
+		FinishedDir:   finishedDir,
+	}
+	r.PendingTasks = make(map[string]string)
+	return r, nil
+}
+
+// GetStatus: return Drakfuv service status (pending tasks, disk free space)
+func (c *Drakvuf) GetStatus() (*StatusResp, error) {
+	r := &StatusResp{
+		PendingTaskNum: len(c.PendingTasks),
+	}
+
+	/* TODO: calculate free disk space and return error if low
+	freeDiskSpace, err := CalculateDiskSpace()
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Alert: Free Disk Space %d", freeDiskSpace))
+		return nil, err
+	}
+	*/
+
+	return r, nil
+}
+
+// submitTask submits a new task to the drakvuf api.
+func (c *Drakvuf) NewTask(fileBytes []byte, fileName string) (string, error) {
+	// calculate MD5 sum on sample content
+	hash := md5.Sum(fileBytes)
+	md5hash := hex.EncodeToString(hash[:])
+
+	// check if task is already pending
+	_, ok := c.PendingTasks[md5hash]
+	if ok {
+		err := errors.New(fmt.Sprintf("Task already pending: ", md5hash))
+		return "", err
+	}
+
+	// check if file exists already
+	_, err := os.Stat(c.IncomingDir + "/" + fileName)
+	if err == nil {
+		err = errors.New(fmt.Sprintf("Sample already exists in incoming"))
+		return "", err
+	}
+
+	// save file to Drakvuf incoming dir
+	err = ioutil.WriteFile(c.IncomingDir+"/"+fileName, fileBytes, 0644)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Error writing sample in incoming"))
+		return "", err
+	}
+
+	// update pending task map
+	c.PendingTasks[md5hash] = fileName
+
+	// md5 hash of the sample is the taskID
+	return md5hash, nil
+}
+
+func (c *Drakvuf) TaskStatus(taskID string) (int, error) {
+	// check if task exists
+	_, ok := c.PendingTasks[taskID]
+	if !ok {
+		err := errors.New(fmt.Sprintf("Task not pending: ", taskID))
+		return 0, err
+	}
+
+	// check if taskID dir exists in Drakvuf finished dir
+	_, err := os.Stat(c.FinishedDir + "/" + taskID)
+	if err != nil {
+		// Not found, task already pending
+		return 0, nil
+	}
+
+	// Task finished
+	return 1, nil
+}
+
+func (c *Drakvuf) TaskReport(taskID string) (*TasksReport, error) {
+	r := &TasksReport{
+		TaskID: taskID,
+	}
+
+	// check if task exists
+	_, ok := c.PendingTasks[taskID]
+	if !ok {
+		err := errors.New(fmt.Sprintf("Task not pending: ", taskID))
+		return nil, err
+	}
+
+	// get Drakvuf result
+	resultBytes, err := ioutil.ReadFile(c.FinishedDir + "/" + taskID + "/drakvuf.log")
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Drakvuf result not found: ", taskID))
+		return nil, err
+	}
+
+	// remove from pending task
+	delete(c.PendingTasks, taskID)
+
+	r.Log = resultBytes
+
+	return r, nil
+}
+
+func (c *Drakvuf) DeleteTask(taskID string) error {
+	return nil
+}
---
+++ services/drakvuf/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/saimoon/Holmes-Totem-Dynamic/services/drakvuf/drakvuf"
+)
+
+const (
+	STORAGE_SAMPLE_DIR string = "/tmp/"
+)
+
+type Config struct {
+	HTTPBinding   string
+	VerifySSL     bool
+	IncomingDir   string
+	ProcessingDir string
+	FinishedDir   string
+	MaxPending    int
+	MaxAPICalls   int
+	LogFile       string
+	LogLevel      string
+}
+
+type Ctx struct {
+	Config  *Config
+	Drakvuf *drakvuf.Drakvuf
+}
+
+type RespStatus struct {
+	Degraded  bool
+	Error     string
+	FreeSlots int
+}
+
+type RespNewTask struct {
+	Error  string
+	TaskID string
+}
+
+type RespCheckTask struct {
+	Error string
+	Done  bool
+}
+
+type RespTaskResults struct {
+	Error   string
+	Results interface{}
+}
+
+var (
+	ctx *Ctx
+)
+
+func main() {
+	// prepare context
+	ctx = &Ctx{
+		Config: &Config{},
+	}
+
+	// parse configuration file
+	cFile, err := os.Open("./service.conf")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	decoder := json.NewDecoder(cFile)
+	err = decoder.Decode(ctx.Config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create Drakvuf object
+	drakvuf, err := drakvuf.New(ctx.Config.IncomingDir, ctx.Config.ProcessingDir, ctx.Config.FinishedDir)
+	if err != nil {
+		panic(err.Error())
+	}
+	ctx.Drakvuf = drakvuf
+
+	// prepare routing
+	r := http.NewServeMux()
+	r.HandleFunc("/status/", HTTPStatus)
+	r.HandleFunc("/feed/", HTTPFeed)
+	r.HandleFunc("/check/", HTTPCheck)
+	r.HandleFunc("/results/", HTTPResults)
+
+	// listening server
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ctx.Config.HTTPBinding,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func HTTPStatus(w http.ResponseWriter, r *http.Request) {
+	resp := &RespStatus{
+		Degraded:  false,
+		Error:     "",
+		FreeSlots: 0,
+	}
+
+	s, err := ctx.Drakvuf.GetStatus()
+	if err != nil {
+		resp.Degraded = true
+		resp.Error = err.Error()
+		HTTP500(w, r, resp)
+		return
+	}
+
+	resp.FreeSlots = ctx.Config.MaxPending - s.PendingTaskNum
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func HTTPFeed(w http.ResponseWriter, r *http.Request) {
+	resp := &RespNewTask{
+		Error:  "",
+		TaskID: "",
+	}
+
+	sample := r.URL.Query().Get("obj")
+	if sample == "" {
+		resp.Error = "No sample given"
+		HTTP500(w, r, resp)
+		return
+	}
+
+	sampleBytes, err := ioutil.ReadFile(STORAGE_SAMPLE_DIR + sample)
+	if err != nil {
+		resp.Error = err.Error()
+		HTTP500(w, r, resp)
+		return
+	}
+
+	taskID, err := ctx.Drakvuf.NewTask(sampleBytes, sample)
+	if err != nil {
+		resp.Error = err.Error()
+		HTTP500(w, r, resp)
+		return
+	}
+	log.Println("HTTPFeed TaskID: ", taskID)
+
+	resp.TaskID = taskID
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func HTTPCheck(w http.ResponseWriter, r *http.Request) {
+	resp := &RespCheckTask{
+		Error: "",
+		Done:  false,
+	}
+
+	taskID := r.URL.Query().Get("taskid")
+	if taskID == "" {
+		resp.Error = "No taskID given"
+		HTTP500(w, r, resp)
+		return
+	}
+
+	s, err := ctx.Drakvuf.TaskStatus(taskID)
+	if err != nil {
+		resp.Error = err.Error()
+		HTTP500(w, r, resp)
+		return
+	}
+
+	resp.Done = (s == 1)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func HTTPResults(w http.ResponseWriter, r *http.Request) {
+	resp := &RespTaskResults{
+		Error: "",
+	}
+
+	taskID := r.URL.Query().Get("taskid")
+	if taskID == "" {
+		resp.Error = "No taskID given"
+		HTTP500(w, r, resp)
+		return
+	}
+
+	report, err := ctx.Drakvuf.TaskReport(taskID)
+	if err != nil {
+		resp.Error = err.Error()
+		HTTP500(w, r, resp)
+		return
+	}
+
+	resp.Results = report.Log
+
+	if err = ctx.Drakvuf.DeleteTask(taskID); err != nil {
+		log.Println("Cleaning drakvuf up failed for task", taskID, err.Error())
+	}
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func HTTP500(w http.ResponseWriter, r *http.Request, response interface{}) {
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(response)
+	return
+}
